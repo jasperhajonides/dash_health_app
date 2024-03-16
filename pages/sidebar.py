e2678f2384c8c92d12a99ae15055663024bfe598
Jasper Hajonides <jasperhajonides@gmail.com>
2024-03-16 18:17:36 +0000

    edit the navbar for the teleport hq code

--- pages/sidebar.py
+++ pages/sidebar.py
@@ -2,17 +2,13 @@ from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 def create_top_navbar():
-    navbar_layout = html.Div(  # This div corresponds to 'landing-page-container' in your CSS
-        [
-            html.Div(  # This div corresponds to 'landing-page-top-container' in your CSS
-                [
-                    dbc.Navbar(
+    navbar_layout =  dbc.Navbar(
                         [
                             # Logo and brand on the left
                             html.A(
                                 dbc.Row(
                                     [
-                                        dbc.Col(html.Img(src="./assets/logo_v1.png", height="50px"), className="my-auto"),
+                                        dbc.Col(html.Img(src="./assets/logo_v1.png", height="84px"), className="my-auto"),
                                         dbc.Col(dbc.NavbarBrand("Vitalics.ai", className="ml-2"), className="my-auto"),
                                     ],
                                     align="center",
@@ -23,15 +19,14 @@ def create_top_navbar():
                             # Right-side elements
                             html.Div(
                                 [
-                                    # Navigation links
                                     html.Div(
                                         [
-                                            dcc.Link("Home", href="/", className="landing-page-text"),
+                                            # dcc.Link("Home", href="/", className="landing-page-text"),
                                             dcc.Link("All Activities", href="/all-activities", className="landing-page-text"),
-                                            dcc.Link("Activities", href="/activities", className="landing-page-text"),
-                                            dcc.Link("Profile", href="/profile", className="landing-page-text"),
-                                            dcc.Link("Nutrition", href="/nutrition", className="landing-page-text"),
-                                            dcc.Link("Information", href="/information", className="landing-page-text"),
+                                            # dcc.Link("Activities", href="/activities", className="landing-page-text01"),
+                                            dcc.Link("Profile", href="/profile", className="landing-page-text01"),
+                                            dcc.Link("Nutrition", href="/nutrition", className="landing-page-text02"),
+                                            dcc.Link("Information", href="/information", className="landing-page-text03"),
                                         ],
                                         className="landing-page-links-container"
                                     ),
@@ -47,15 +42,14 @@ def create_top_navbar():
                                 className="landing-page-right-side"  # This div corresponds to 'landing-page-right-side' in your CSS
                             ),
                         ],
-                        className="landing-page-navbar",  # Corresponds to 'landing-page-navbar' in your CSS
-                        color="light",
+                        className="landing-page-navbar",  # Use your custom CSS class
                         dark=False,
                         sticky="top",
                     ),
-                ],
-                className="landing-page-top-container",
-            ),
-        ],
-        className="landing-page-container",
-    )
+                # ],
+            #     className="landing-page-top-container",
+            # ),
+        # ],
+    #     className="landing-page-container",
+    # )
     return navbar_layout
